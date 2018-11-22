f19887ba51106204d3ba1fd3bc4fb0d29e553881
Azharuddin Syed <anqazi123@gmail.com>
2018-11-22 14:26:48 +0530

    Work Around

---
+++ Test Suites/Regression.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-19T11:46:36</lastRun>
+   <mailRecipient>azharuddin.syed@amla.io;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>48788069-a20e-44a8-9c7c-cb04ddbf7835</testSuiteGuid>
+   <testCaseLink>
+      <guid>b476cd78-61da-4674-ba1c-713383178b2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1_Open BrowserWith Web UrL</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
